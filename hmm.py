ab6bc1c7270e304e4f06bc2085d2a0dd4378efc2
raoqiyu <raoqiyu@yeah.net>
2019-05-05 14:17:40 +0800

    forward and backward algorithm

--- hmm.py
+++ hmm.py
@@ -5,37 +5,69 @@
 
 import numpy as np
 
-np.array
 
 class HMM:
-    def __init__(self):
-        pass
+    def __init__(self, M, V):
+        """
+
+        Parameters:
+        m - the number of hidden states
+
+        """
+
+        self.M = M
+        self.V = V
+
+        self.pi = np.ones(self.M) / self.M # initial state distribution
+
+        self.A = np.random.random((self.M, self.M)) # state transition matrix
+        self.A /= self.A.sum(axis=1,keepdims=True)
+
+        self.B = np.random.random((self.M,self.V)) # observed state matrix
+        self.B /= self.B.sum(axis=1, keepdims=True)
 
     def forward(self, x):
         """
         the forward part of the forward-backward algorithm
-        calculate forward probability
+        calculate forward probability: the probability of observed sequence at time t is  (x1,x2,x3...,xt) and the state
+            at time t is qi given model λ
+        α_t(i) = P(x1,x2,...xt,i_t=q_i | λ)
+
 
         Parameters:
-        x -  observed sequence
+        x - observed sequence, np.array, T*1
         Returns:
-        alpha -   forward probability
+        alpha - forward probability
         """
+        T = x.shape[0]
 
-        pass
+        alpha = np.zeros((x.shape[0],self.M))
+        alpha[0] = self.pi*self.B[:,x[0]]
+        for t in range(1,T):
+            alpha[t] = alpha[t-1].dot(self.A) * self.B[:,x[t]]
+
+        return alpha
 
     def backward(self, x):
         """
         the backward part of the forward-backward algorithm
-        calculate backward probability
+        calculate backward probability: the probability of the observed sequence from time t+1 to T is
+            (x_t+1,x_t+2,...,x_T) given state is qi at time t and the model λ
+        α_t(i) = P(x_t+1,x_t+2,...x_T|λ, i_t=q_i )
 
         Parameters:
-        x -  observed sequence
+        x - observed sequence
         Returns:
-        beta -   backward probability
+        beta - backward probability
         """
+        T = x.shape[0]
 
-        pass
+        beta = np.zeros((T,self.M))
+        beta[-1] = 1
+        for t in range(T-2,-1,-1):
+            beta[t] = self.A.dot(self.B[:,x[t]]) * beta[t+1]
+
+        return beta
 
     def viterbi(self,x):
         """
